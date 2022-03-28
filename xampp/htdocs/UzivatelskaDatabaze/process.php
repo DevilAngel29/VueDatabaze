3f4f6bce5eb4378ee1ece55e93a8b2ac9f62c6cf
Oleg <o.borman29@gmail.com>
2022-03-28 07:24:18 +0200

    uprava pres prettier

--- xampp/htdocs/UzivatelskaDatabaze/process.php
+++ xampp/htdocs/UzivatelskaDatabaze/process.php
@@ -1,42 +1,41 @@
 <?php
-$conn = new mysqli("localhost", "root","","zakaznickadatabaze");
-if($conn->connect_error){
-    die("Connection Failed!" .$conn->connect_error);
+$conn = new mysqli("localhost", "root", "", "zakaznickadatabaze");
+if ($conn->connect_error) {
+    die("Connection Failed!" . $conn->connect_error);
 }
 
-$result = array('error'=>false);
+$result = array('error' => false);
 $action = '';
 
-if(isset($_GET['action'])){
+if (isset($_GET['action'])) {
     $action = $_GET['action'];
 }
 
-if($action == 'read'){
+if ($action == 'read') {
     $sql = $conn->query("SELECT * FROM users");
     $users = array();
-    while($row = $sql->fetch_assoc()){
+    while ($row = $sql->fetch_assoc()) {
         array_push($users, $row);
     }
     $result['users'] = $users;
 }
 
-if($action == 'create'){
+if ($action == 'create') {
     $jmeno = $_POST['jmeno'];
     $prijmeni = $_POST['prijmeni'];
     $adresa = $_POST['adresa'];
 
     $sql = $conn->query("INSERT INTO users (jmeno, prijmeni,adresa) VALUES('$jmeno', '$prijmeni', '$adresa')");
 
-   if($sql) {
-       $result['message'] ="Zakaznik uspesne pridan!";
-   }
-   else{
-       $result['error'] = true;
-       $result['message'] = "Nelze pridat zakaznika!";
-   }
+    if ($sql) {
+        $result['message'] = "Zakaznik uspesne pridan!";
+    } else {
+        $result['error'] = true;
+        $result['message'] = "Nelze pridat zakaznika!";
+    }
 }
 
-if($action == 'update'){
+if ($action == 'update') {
     $id = $_POST['id'];
     $jmeno = $_POST['jmeno'];
     $prijmeni = $_POST['prijmeni'];
@@ -44,29 +43,26 @@ if($action == 'update'){
 
     $sql = $conn->query("UPDATE users SET jmeno='$jmeno', prijmeni='$prijmeni', adresa='$adresa' WHERE id='$id'");
 
-   if($sql) {
-       $result['message'] ="Zakaznik uspesne upraven!";
-   }
-   else{
-       $result['error'] = true;
-       $result['message'] = "Nelze upravit zakaznika!";
-   }
+    if ($sql) {
+        $result['message'] = "Zakaznik uspesne upraven!";
+    } else {
+        $result['error'] = true;
+        $result['message'] = "Nelze upravit zakaznika!";
+    }
 }
 
-if($action == 'delete'){
+if ($action == 'delete') {
     $id = $_POST['id'];
 
     $sql = $conn->query("DELETE FROM users WHERE id='$id'");
 
-   if($sql) {
-       $result['message'] ="Zakaznik uspesne smazan!";
-   }
-   else{
-       $result['error'] = true;
-       $result['message'] = "Nelze smazat zakaznika!";
-   }
+    if ($sql) {
+        $result['message'] = "Zakaznik uspesne smazan!";
+    } else {
+        $result['error'] = true;
+        $result['message'] = "Nelze smazat zakaznika!";
+    }
 }
 
 $conn->close();
 echo json_encode($result);
-?>
